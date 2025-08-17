cc659b1368a79eb95d1d5881b00a1ec9531b9ad1
alishaunair <jihan.alisha14@gmail.com>
2025-08-17 23:36:58 +0700

    Restore fiturPertama.py dari fitur-pertama

---
+++ fiturPertama.py
@@ -0,0 +1,9 @@
+# fitur_pertama.py
+# Contoh file Python sederhana untuk latihan Git & GitHub
+
+def salam(nama):
+    return f"Halo, {nama}! Selamat belajar Git dan GitHub 🚀"
+
+if __name__ == "__main__":
+    pengguna = input("Masukkan nama Anda: ")
+    print(salam(pengguna))
